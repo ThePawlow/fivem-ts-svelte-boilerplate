ef994181f2518ac8080ab5c6740800c9a559de0d
ThePawlow <thepawlow@proton.me>
2025-02-23 14:55:18 +0100

    Integrated nativewrapper clientside

--- src/client/main.ts
+++ src/client/main.ts
@@ -1,13 +1,31 @@
 import 'reflect-metadata';
 import { container } from "./container";
 import {DebugController} from "./controllers/DebugController";
+import {Game, Model, Vehicle, VehicleSeat, World} from "@nativewrappers/fivem";
+import {LogService} from "../shared/services/LogService";
 
-console.log("Resource is starting...");
-
-// Retrieve controller from Inversify
+const logger = container.get<LogService>(LogService);
 const debugController = container.get<DebugController>(DebugController);
 
-// FiveM event handling
 on("gameEventTriggered", (name: string) => {
     debugController.onPlayerConnecting(name);
 });
+
+RegisterCommand(
+    "adder",
+    async (source, args) => {
+        const playerCoords = Game.PlayerPed.Position;
+        const vehicle = await World.createVehicle(
+            new Model("adder"),
+            playerCoords,
+            4,
+        );
+        if (vehicle instanceof Vehicle) {
+            Game.PlayerPed.setIntoVehicle(vehicle, VehicleSeat.Driver);
+            logger.log(`Spawned Adder for ${Game.Player.Name}`)
+        }
+    },
+    false,
+);
+
+logger.log("Resource started.");
