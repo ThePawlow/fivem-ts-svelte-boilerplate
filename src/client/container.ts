7b7744f1bd7cf907d5af8ab0bb54191bc1892c46
Ramon Pawlowski <43045790+ThePawlow@users.noreply.github.com>
2025-02-27 09:50:13 +0100

    Dev (#10)

--- src/client/container.ts
+++ src/client/container.ts
@@ -1,10 +1,10 @@
 import { Container } from 'inversify';
 import {LogService} from "../shared/services/LogService";
 import {DebugController} from "./controllers/DebugController";
+import {NUIController} from "./controllers/NUIController";
 
 const container: Container = new Container();
-
 container.bind(LogService).toSelf();
-
+container.bind(NUIController).toSelf();
 container.bind(DebugController).toSelf();
 export { container };
