ca6bb7d8154fd3bb63b342e85b9834c40233d3bb
ramon.pawlowski <ramon.pawlowski@ise.de>
2025-03-12 11:57:48 +0100

    feat(nui) fixing invalid .env

--- src-nui/vite.config.ts
+++ src-nui/vite.config.ts
@@ -3,13 +3,13 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 import * as path from "node:path";
 import dotenv from 'dotenv'
 dotenv.config({
-  path: path.resolve(__dirname, '../../.env')
+  path: path.resolve(__dirname, '../.env')
 })
 
 export default defineConfig({
   plugins: [svelte()],
   base: './',
   build: {
-    outDir: path.resolve(__dirname, process.env.OUTPUT_FOLDER! + "/nui")
+    outDir: path.resolve(__dirname, "../" + process.env.OUTPUT_FOLDER + "/nui")
   }
 })
