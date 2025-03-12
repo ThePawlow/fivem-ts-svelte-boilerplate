9e5c228ed326f8a161280b36338395e63373975e
ramon.pawlowski <ramon.pawlowski@ise.de>
2025-03-12 15:30:26 +0100

    fix(StartUp) Using Player and DatabaseController in startup.ts

--- src/server/startup.ts
+++ src/server/startup.ts
@@ -1,18 +1,18 @@
 import 'reflect-metadata';
 import { container } from "./container";
 import { PlayerController } from "./controllers/PlayerController";
-import {LogService} from "../shared/services/LogService";
-import {Events} from "@nativewrappers/server";
+import { LogService } from "../shared/services/LogService";
+import { Events } from "@nativewrappers/server";
+import { DatabaseController } from './controllers/DatabaseController';
 
-const logger = container.get<LogService>(LogService);
-const playerController = container.get<PlayerController>(PlayerController);
+const logger = container.get(LogService);
+const dbController = container.get(DatabaseController);
+const playerController = container.get(PlayerController)
+
+logger.log(dbController.Client.account.count.toString())
 
 Events.on("playerConnecting", (name: string) => {
-    logger.log(`Player connected with name ${name}`);
-})
-
-Events.on("playerDisconnected", (name: string) => {
-    logger.log(`Player disconnected with name ${name}`);
+    playerController.onPlayerConnecting(name);
 })
 
 logger.log("Server is starting...");
